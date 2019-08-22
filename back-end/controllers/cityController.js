a32e537f74628c7d9fbd70103b9f760f00837684
christianvilla <christianj.villa@gmail.com>
2019-08-22 14:51:27 -0700

    adds post crud

--- back-end/controllers/cityController.js
+++ back-end/controllers/cityController.js
@@ -1,41 +1,41 @@
 // NOTE access to database in models directory
 const db = require('../models');
 //NOTE  acquires refractored functions for error and success responses
-const responseMessage = require('./response');
+const { sendErrorResponse, sendSuccessResponse } = require('./response');
 
 
 const index = (req, res) => {
     db.City.find({}).exec((error, foundAllCities) => {
-        if (error) return responseMessage.sendErrorResponse(res, error);
-        responseMessage.sendSuccessResponse(res, foundAllCities);
+        if (error) return sendErrorResponse(res, error);
+        sendSuccessResponse(res, foundAllCities);
     });
 };
 
 const show = (req, res) => {
     db.City.findById({ _id: req.params._id }, (error, foundOneCity) => {
-        if (error) return responseMessage.sendErrorResponse(res, error);
-        responseMessage.sendSuccessResponse(res, foundOneCity);
+        if (error) return sendErrorResponse(res, error);
+        sendSuccessResponse(res, foundOneCity);
     });
 };
 
 const create = (req, res) => {
     db.City.create(req.body, (error, createdCity) => {
-        if (error) return responseMessage.sendErrorResponse(res, error);
-        responseMessage.sendSuccessResponse(res, createdCity);
+        if (error) return sendErrorResponse(res, error);
+        sendSuccessResponse(res, createdCity);
     });
 };
 
 const update = (req, res) => {
     db.City.findByIdAndUpdate(req.params._id, req.body, { new: true }, (error, updatedCity) => {
-        if (error) return responseMessage.sendErrorResponse(res, error);
-        responseMessage.sendSuccessResponse(res, updatedCity);
+        if (error) return sendErrorResponse(res, error);
+        sendSuccessResponse(res, updatedCity);
     });
 };
 
 const del = (req, res) => {
     db.City.findByIdAndDelete({ _id: req.params._id }, (error, deletedCity) => {
-        if (error) return responseMessage.sendErrorResponse(res, error);
-        responseMessage.sendSuccessResponse(res, deletedCity);
+        if (error) return sendErrorResponse(res, error);
+        sendSuccessResponse(res, deletedCity);
     })
 }
 
--- back-end/controllers/response.js
+++ back-end/controllers/response.js
@@ -1,10 +1,12 @@
-// NOTE functions for success and error responses and exports outside of page.
+
 const sendSuccessResponse = (res, data) => {
 	res.status(200).json({
 		status: 201,
 		data: data
 	});
 };
+
+
 const sendErrorResponse = (res, error) => {
 	console.log({ error });
 	res.status(400).json({
@@ -12,7 +14,7 @@ const sendErrorResponse = (res, error) => {
 		message: 'Something went wrong, please try again'
 	});
 };
-​
+
 module.exports = {
     sendSuccessResponse,
     sendErrorResponse,
