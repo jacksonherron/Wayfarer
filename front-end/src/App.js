3acd33bc8f45871c926034963f3047e7230e76a8
jacksonherron <jacksonherron@gmail.com>
2019-08-22 10:12:15 -0700

    Adds login and register modals

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './auth/Login/Login';
+import Register from './auth/Register/Register';
+import NavBar from './layout/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Register />
+      <Login />
     </div>
   );
 }
---
+++ front-end/src/auth/Register/Register.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#registerModal">
+            Register
+            </button>
+            
+            <div className="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Register</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            ...
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Register;
