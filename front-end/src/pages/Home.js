ce4630f76ec60850a88024b03988640b699a624a
christianvilla <christianj.villa@gmail.com>
2019-08-22 16:40:27 -0700

    adds carousel component

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Register from '../components/auth/Register';
 import Login from '../components/auth/Login';
+import Carousel from '../components/Carousel/Carousel';
 
 const Home = ({currentUser, setCurrentUser}) => {
     return (
         <>
             <Register />
             <Login setCurrentUser={setCurrentUser}/>
+            <Carousel />
         </>
     );
 };
