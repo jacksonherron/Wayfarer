737127791ce99cfcf1221c1e0b0a10b50ea2aafb
jameswang <jameswangdr@gmail.com>
2019-08-22 10:09:36 -0700

    Models.2

---
+++ back-end/models/index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/auth-api-sei3';
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+})
+  .then(() => console.log('MongoDB connected successfully...!'))
+  .catch((err) => console.log(`MongoDB connection error: ${err}`));
+
+module.exports = {
+  User: require('./User'),
+  City: require('./City'),
+  Post: require('./Post'),
+};
