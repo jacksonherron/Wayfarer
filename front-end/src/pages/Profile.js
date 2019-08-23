e82e8f895273a47291631cb554bab32c1bf687cc
Azamat Baiyzbekov <azamatbaiyzbekov@Azamats-MacBook-Pro.local>
2019-08-23 10:17:08 -0700

    adds profile

--- front-end/src/pages/Profile.js
+++ front-end/src/pages/Profile.js
@@ -1,4 +1,17 @@
 import React from 'react';
 import ProfileContainer from '../containers/ProfileContainer';
 
-const Profile = ({ profile: { username}})
+const Profile = ({ profile: { username , email, profile_photo, join_date, location}}) => {
+    return (
+        <div>
+            <p><strong>Username</strong></p>
+            <p><strong>Email</strong></p> 
+            <p><strong>Photo</strong></p>  
+            <p><strong>Join Date</strong></p> 
+            <p><strong>Location</strong></p> 
+        </div>
+
+    );
+};
+
+export default Profile;
