23aace8d7ae444ba496720c634d39343c7c6f649
jacksonherron <jacksonherron@gmail.com>
2019-08-23 11:18:31 -0700

    Fixes bug

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PostsContainer from '../containers/PostsContainer';
 
-class Home {
-
+class Home extends Component {
     render() {
         return (
             <>
