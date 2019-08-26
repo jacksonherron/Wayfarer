a279b5e4b3b5d6956c2378327ff7dfead2030c41
jacksonherron <jacksonherron@gmail.com>
2019-08-26 09:55:04 -0700

    Refactors profile page

--- front-end/src/containers/ProfileContainer.js
+++ front-end/src/containers/ProfileContainer.js
@@ -1,50 +1,96 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Profile from '../pages/Profile';
-import { API_URL } from '../constants';
+import React from 'react';
 
 
-class ProfileContainer extends Component {
-    state = {
-        profile: {
-            username: '',
-            email: '',
-            profile_photo: '',
-            join_date: '',
-            location: '',
-        }
-        
-    };
-
-    componentDidMount() {
-        const userId = localStorage.getItem('uid');
-        axios.get(`${API_URL}/users/${userId}`, { withCredentials: true })
-            .then(res => this.setState({ profile: res.data.data }))
-            .catch(err => console.log(err));
-    };
-
-   
-  
+import './ProfileContainer.css'
 
 
+const ProfileContainer = ( { profile: { username , email, profile_photo, join_date, location } } ) => {
+    return (
 
+    <>
 
-
-
-
-
-
-    render() {
-        return (
-            
-            <Profile profile={this.state.profile}  />
+    <div className="container emp-profile">
+        <form  method="post" encType="multipart/form-data" >
+            <div className="row">
+                <div className="col-md-4">
+                    <div className="profile-img">
+                        <img src={profile_photo} alt="user"/>
+                            <div className="file btn btn-lg btn-primary">
+                                Change Photo
+                                <label for="file"></label>
+                                <input type="file" id="upload" name="file" accept="image/*"  />
+                                
             
             
-            
-        )
-    }
+                                </div>
+                                <div>
 
-}
+                                </div>
+                                
+                    </div>
+                </div>
+            <div className="col-md-6">
+                <div className="profile-head">
+                    <h5>
+                        <p><strong>Username:</strong> {username}</p>
+                    </h5>
+                <ul className="nav nav-tabs" id="myTab" role="tablist">
+                    <li className="nav-item">
+                        <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
+                    </li>
+                    <li className="nav-item">
+                    </li>
+                </ul>
+                </div>
+            </div>
+            <div className="col-md-2">
+
+            </div>
+            </div>
+
+
+    <div className="col-md-8">
+        <div className="tab-content profile-tab" id="myTabContent">
+            <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                <div className="row">
+    <div className="col-md-6">
+    <label>Email:</label>
+    </div>
+    <div className="col-md-6">
+    <p>{email}</p>
+    </div>
+    </div>
+            </div>
+    <div className="row">
+        <div className="col-md-6">
+            <label>Join Date:</label>
+            </div>
+    <div className="col-md-6">
+        <p>{new Date(join_date).toLocaleDateString()}</p>
+        </div>
+    </div>
+        <div className="row">
+            <div className="col-md-6">
+                <label>Location:</label>
+            </div>
+    <div className="col-md-6">
+            <p>{location}</p>
+            
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+
+</form>      
+    
+</div>
+
+     </>
+);
+};
 
 
 export default ProfileContainer;
