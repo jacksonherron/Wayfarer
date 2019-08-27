4a275c7f32f22081906c8b0df2b4b0d2a76bcbc4
jameswang <jameswangdr@gmail.com>
2019-08-27 08:57:26 -0700

    landing fin

--- front-end/src/components/Landing/Landing.js
+++ front-end/src/components/Landing/Landing.js
@@ -8,21 +8,21 @@ const Landing = () => {
     <div className="container landing">
       <div className="row">
         <div className="col-lg-4">
-          <img className="rounded-circle" src="https://media.licdn.com/dms/image/C4E03AQGPuNbxrKiG0w/profile-displayphoto-shrink_800_800/0?e=1571875200&v=beta&t=MR0JcOTAdvDjvzWhjUp6y2NQhxIqbpk5Ashysaiz5bI" alt="James" width="140" height="140" />
-          <h2>"Vacation"</h2>
+          <img className="rounded-circle" src="https://cdn4.iconfinder.com/data/icons/travel-2-11/512/travel-tourism-vacation-holiday-05-512.png" alt="Vacation" width="140" height="140" />
+          <h2>Vacation</h2>
           {/* <p>I travel a ton. Thanks to Wayfarer I can find the cities with the best beaches. Soaking up the sun is what I live for!</p> */}
-          <p>Find the cities with the perfect beach to soak up the sun.</p>
+          <p>Find the cities for the most perfect getaway</p>
 
         </div>
         <div className="col-lg-4">
-          <img className="rounded-circle" src="https://media.licdn.com/dms/image/C5603AQE2ChwqXF8qPQ/profile-displayphoto-shrink_800_800/0?e=1571875200&v=beta&t=aeIY7TxPgs4aXxrruFtCHsNRGfrl5IGA8J3BEEQLcAM" alt="Jackson" width="140" height="140" />
-          <h2>"True Love"</h2>
+          <img className="rounded-circle" src="https://cdn3.iconfinder.com/data/icons/virtual-notebook/16/button_connect-512.png" alt="Connect" width="140" height="140" />
+          <h2>Connect</h2>
           {/* <p>Wayfarer helped me find the love of my life in the city of love, Paris!</p> */}
-          <p>Meet and travel with people from all around the globe! Maybe even find your true love? </p>
+          <p>Meet and travel with people from all around the globe!</p>
         </div>
         <div className="col-lg-4">
-          <img className="rounded-circle" src="https://media.licdn.com/dms/image/C5603AQGqiGztI2yfxg/profile-displayphoto-shrink_200_200/0?e=1571875200&v=beta&t=1-FNJuu9OkUZtI3pKE5cz40xuQyAa1M2-jDfuucFRTI" alt="Christian" width="140" height="140" />
-          <h2>"Gorgeous Views"</h2>
+          <img className="rounded-circle" src="https://cdn0.iconfinder.com/data/icons/photo-cameras/164/olympus-35dc2x-512.png" alt="Camera" width="140" height="140" />
+          <h2>Share</h2>
           {/* <p>I love using Wayfarer! I get to find the cities that have incredible views!</p> */}
           <p>Capture the most beautiful photos that mother nature has to offer</p>
         </div>
